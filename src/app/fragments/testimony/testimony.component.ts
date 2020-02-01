97f980e89d1f1230b4af8554465d9f2c47d44da4
Ashot Galstyan <ash.galstyan@gmail.com>
2020-02-01 17:55:55 +0400

    Added testymonials and carousel component

---
+++ src/app/fragments/testimony/testimony.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testimony',
+  templateUrl: './testimony.component.html',
+  styleUrls: ['./testimony.component.scss']
+})
+export class TestimonyComponent implements OnInit {
+  config: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
